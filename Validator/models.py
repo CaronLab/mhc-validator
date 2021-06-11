1b5df80b077cefd8c8a88de745fc144c67ea39f3
kevin <kkovalchik.chusj@gmail.com>
2021-06-11 09:28:00 -0400

    update model layers

--- Validator/models.py
+++ Validator/models.py
@@ -89,26 +89,28 @@ def get_model_with_lstm_peptide_encoding(ms_feature_length: int,
     return model
 
 
-def get_bigger_model_with_peptide_encoding2(ms_feature_length: int, max_pep_length: int = 15):
+def get_bigger_model_with_peptide_encoding2(ms_feature_length: int, max_pep_length: int = 15, dropout: float = 0.5):
     pep_input = keras.Input(shape=(max_pep_length, 21))
-    p = layers.Conv1D(8, 8, padding="valid", activation=tf.nn.tanh)(pep_input)
+    p = layers.Conv1D(12, 4, padding="valid", activation=tf.nn.tanh)(pep_input)
     #p = layers.Conv1D(8, 4, padding="valid", activation=tf.nn.tanh)(p)
     #p = layers.Dropout(0.3)(p)
     p = layers.MaxPool1D()(p)
-    p = layers.Dropout(0.5)(p)
+    p = layers.Dropout(dropout)(p)
     p = layers.Flatten()(p)
-    pep_out_flat = layers.Dense(6, activation=tf.nn.relu)(p)
+    pep_out_flat = layers.Dense(4, activation=tf.nn.relu)(p)
     #pep_out_flat = layers.Dropout(0.2)(pep_out_flat)
 
     ms_feature_input = keras.Input(shape=(ms_feature_length,))
     ms_plus_pep = layers.concatenate([ms_feature_input, pep_out_flat])
-    n_nodes = int(int(ms_plus_pep.shape[1]) * 1.5)
+    n_nodes = int(int(ms_plus_pep.shape[1]) * 3)
     x = layers.Dense(n_nodes, activation=tf.nn.relu)(ms_plus_pep)
-    x = layers.Dropout(0.5)(x)
+    x = layers.Dropout(dropout)(x)
     x = layers.Dense(n_nodes, activation=tf.nn.relu)(x)
-    x = layers.Dropout(0.5)(x)
+    x = layers.Dropout(dropout)(x)
     x = layers.Dense(n_nodes, activation=tf.nn.relu)(x)
-    x = layers.Dropout(0.5)(x)
+    x = layers.Dropout(dropout)(x)
+    x = layers.Dense(n_nodes, activation=tf.nn.relu)(x)
+    x = layers.Dropout(dropout)(x)
     output = layers.Dense(1, activation=tf.nn.sigmoid)(x)
     #ms_out_flat = layers.Flatten()(x)
 
@@ -122,15 +124,17 @@ def get_bigger_model_with_peptide_encoding2(ms_feature_length: int, max_pep_leng
     return model
 
 
-def get_model_without_peptide_encoding(ms_feature_length: int, max_pep_length: int):
-    n_nodes = ms_feature_length * 2
+def get_model_without_peptide_encoding(ms_feature_length: int, max_pep_length: int, dropout: float = 0.4):
+    n_nodes = ms_feature_length * 3
     input = keras.Input(shape=(ms_feature_length,))
     x = layers.Dense(n_nodes, activation=tf.nn.relu)(input)
-    x = layers.Dropout(0.5)(x)
+    x = layers.Dropout(dropout)(x)
     x = layers.Dense(n_nodes, activation=tf.nn.relu)(x)
-    x = layers.Dropout(0.5)(x)
+    x = layers.Dropout(dropout)(x)
     x = layers.Dense(n_nodes, activation=tf.nn.relu)(x)
-    x = layers.Dropout(0.5)(x)
+    x = layers.Dropout(dropout)(x)
+    x = layers.Dense(n_nodes, activation=tf.nn.relu)(x)
+    x = layers.Dropout(dropout)(x)
     output = layers.Dense(1, activation=tf.nn.sigmoid)(x)
 
     model = keras.Model(inputs=input, outputs=output)
