e39934af2cfb182c1f61a7a16fb9be01749f1d4f
kevinkovalchik <kevin.kovalchik@gmail.com>
2022-01-26 09:14:19 -0500

    add function to encode modified amino acids as numbers (for AutoRT)

--- mhcvalidator/peptides.py
+++ mhcvalidator/peptides.py
@@ -1,9 +1,11 @@
 import re
-from typing import Union, List
+from typing import Union, List, Type
 from copy import deepcopy
+import numpy as np
+from tqdm import tqdm
 
 
-def remove_modifications(peptides: Union[List[str], str]):
+def remove_modifications(peptides: Union[List[str], str, Type[np.array]]):
     if isinstance(peptides, str):
         peptide = ''.join(re.findall('[a-zA-Z]+', peptides))
         if peptide.startswith('n'):
@@ -24,7 +26,7 @@ def remove_modifications(peptides: Union[List[str], str]):
     return unmodified_peps
 
 
-def remove_previous_and_next_aa(peptides: Union[List[str], str]):
+def remove_previous_and_next_aa(peptides: Union[List[str], str, Type[np.array]]):
     peptides = deepcopy(peptides)
     return_one = False
     if isinstance(peptides, str):
@@ -38,6 +40,38 @@ def remove_previous_and_next_aa(peptides: Union[List[str], str]):
     if return_one:
         return peptides[0]
     return peptides
+
+
+def encode_peptide_modifications(peptides: Union[List[str], Type[np.array]], modification_encoding: dict = None,
+                                 return_encoding_dictionary: bool = False):
+    peptides = deepcopy(peptides)
+    peptides = np.array(peptides)
+    peptides = remove_previous_and_next_aa(peptides)
+
+    if modification_encoding is not None:  # a dictionary containing modification encoding was passed
+        mod_dict = modification_encoding
+    else:  # we need to build the modification encoding dictionary
+        modifications = []
+        for pep in peptides:
+            mods = re.findall('([a-zA-z][[({][0-9.a-zA-Z]+[](}])', pep)
+            modifications += mods
+        modifications = set(modifications)
+
+        mod_dict = {}
+        for i, mod in enumerate(modifications):
+            mod_dict[mod] = str(i + 1)
+
+    # replace the modifications in the peptide strings with the number encodings
+    for i, pep in enumerate(peptides):
+        for mod in mod_dict.keys():
+            while mod in pep:
+                pep = pep.replace(mod, mod_dict[mod])
+        peptides[i] = pep
+
+    if return_encoding_dictionary:
+        return peptides, mod_dict
+    else:
+        return peptides
 
 
 def clean_peptide_sequences(peptides: List[str]) -> List[str]:
